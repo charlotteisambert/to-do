d895459fe6e1fc20bafa0b70c104c8dba61ceb7e
Charlotte Isambert <charlotte.i@amusenetwork.com>
2020-06-06 18:09:02 +0200

    Updates

---
+++ precache-manifest.487c644b6dcd6f3cb10bdde13284513d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "71268c1669538240282d33164d1cf852",
+    "url": "/to-do/index.html"
+  },
+  {
+    "revision": "254e743e1756b3147cc6",
+    "url": "/to-do/static/js/2.73380bbd.chunk.js"
+  },
+  {
+    "revision": "fbb6e99068dfb0db4c7511bdc3c130cc",
+    "url": "/to-do/static/js/2.73380bbd.chunk.js.LICENSE"
+  },
+  {
+    "revision": "f92e78e089287ee3beab",
+    "url": "/to-do/static/js/main.c18a5f7d.chunk.js"
+  },
+  {
+    "revision": "ed991103ccdddb0ab5c1",
+    "url": "/to-do/static/js/runtime-main.bbf44f33.js"
+  }
+]);
